8d91abd2b44d00f2e32513d0be37d3c6613eea25
thesanath <sanathsalampuria4@gmail.com>
2026-04-03 22:43:58 +0530

    Mobile app

--- templates/core/service_worker.js
+++ templates/core/service_worker.js
@@ -1,9 +1,9 @@
 {% load static %}
-const CACHE_NAME = "clubshub-pwa-v14";
+const CACHE_NAME = "clubshub-pwa-v15";
 const OFFLINE_URL = "{% url 'core:offline' %}";
 const STATIC_ASSETS = [
   OFFLINE_URL,
-  "{% static 'css/app.css' %}?v=20260403-mobile-topbar-glowup",
+  "{% static 'css/app.css' %}?v=20260403-mobile-search-actions-row",
   "{% static 'js/app.js' %}?v=20260403-mobile-keyboard-gap-fix",
   "{% static 'logo/logo1.png' %}",
   "{% static 'icons/favicon-64.png' %}?v=20260403-logo1-branding",
